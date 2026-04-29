3aea11b92b5a623cffde18eabfc6859567f55986
FoyerD <amitfoye@post.bgu.ac.il>
2026-04-29 15:31:30 +0300

    added readme

--- scripts/measure_nothing.sh
+++ scripts/measure_nothing.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
-export PATH="/home/debian/anaconda3/envs/EM/bin/:/home/debian/repos/pinpoint/build:$PATH"  
-OUT_DIR="out_files/exp_baseline"
+OUT_DIR=""
+NUM_EXPS=1
+SETUP_FILE=""
+SETUP_COMMAND=cp
+PYTHON_COMMAND=""
+
+
+while getopts "o:n:p:" opt; do
+  case "$opt" in
+	o)
+		OUT_DIR="$OPTARG/exp_baseline"
+		;;
+    
+	n)
+		NUM_EXPS=$OPTARG
+		;;
+    
+    p)
+        PYTHON_COMMAND="$OPTARG"
+        ;;
+  esac
+done
+
 mkdir -p $OUT_DIR
 sudo chmod a+w,a+r $OUT_DIR
 OUT_FILE=$OUT_DIR/raw.txt
 
-pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python nothing.py -o$OUT_DIR -t$1
+pinpoint -c --timestamp -r $NUM_EXPS -e rapl:pkg,GPU -o $OUT_FILE -- $PYTHON_COMMAND nothing.py -o$OUT_DIR -t$1
 chmod a+w,a+r $OUT_FILE
